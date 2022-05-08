a62bc38acebe16188849043526cdf98561d56f19
Chunjie Nan <60551793+nancunjie4560@users.noreply.github.com>
2022-05-08 17:18:23 -0400

    Update 01answer.js

--- module6/d3_lab/01answer.js
+++ module6/d3_lab/01answer.js
@@ -1,8 +1,22 @@
 d3.csv('ue_industry.csv', data => {
-
-    // Define your scales and generator here.
+        
+    console.log(data);
+        
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.index))
+        .range([1180, 20]);
+            
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.Agriculture))
+        .range([580, 20]);
+        
+    const line = d3.line()
+        .x(d => xScale(+d.index))
+        .y(d => yScale(+d.Agriculture));
 
     d3.select('#answer1')
-        // append more elements here
+        .append('path')
+            .attr('d', line(data))
+            .attr('stroke', '#2e2928');
 
-});
+        }); 
